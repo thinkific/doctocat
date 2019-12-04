2a43494d005c8df7ba2df5e23481835f179f3715
Grant Lee <highestblue@gmail.com>
2019-12-04 00:35:50 -0800

    change folder structure

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++
@@ -1,4 +0,0 @@
-import * as primerComponents from '@primer/components'
-import * as doctocatComponents from '@primer/gatsby-theme-doctocat'
-
-export default {...primerComponents, ...doctocatComponents}
